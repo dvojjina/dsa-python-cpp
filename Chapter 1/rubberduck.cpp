750cdf4086c1212e062fd2233385bfb38a4103e3
Darya Vojjina <darya.vojjina@ucdenver.edu>
2025-01-06 10:47:09 -0700

    Chapter 1 Intro to CPP

---
+++ Chapter 1/rubberduck.cpp
@@ -0,0 +1,12 @@
+/* Ever heard of rubber duck debugging?
+    See https://en.wikipedia.org/wiki/Rubber_duck_debugging */
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout << "Ever heard of rubber duck debugging?" << endl;
+  cout << "                __     " << endl;
+  cout << "              <(o )___-" << endl;
+  cout << "               ( .__> /" << endl;
+  cout << "                `----' " << endl;
+}
